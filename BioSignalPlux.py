cf0ca35a764a2939690cccadd4ec48a50e079f53
Gregory-Marshall <90696781+Gregory-Marshall@users.noreply.github.com>
2021-12-01 15:51:16 -0500

    Added fNIRS output to Sp02

--- BioSignalPlux.py
+++ BioSignalPlux.py
@@ -2,20 +2,27 @@ import json
 
 from scipy.signal import filtfilt
 
-print(".",end="")
+print(".", end="")
 from tkinter import Tk
-print(".",end="")
+
+print(".", end="")
 from tkinter.filedialog import askopenfilename
-print(".",end="")
+
+print(".", end="")
 import matplotlib.pyplot as plt
-print(".",end="")
+
+print(".", end="")
 import numpy as np
-print(".",end="")
+
+print(".", end="")
 import pandas as pd
-print(".",end="")
+
+print(".", end="")
 from Spectrogram import plotSpectrogram
-print(".",end="")
+
+print(".", end="")
 print("Libraries loaded")
+
 
 def getEOH(path):
     tmp = open(path)
@@ -39,7 +46,8 @@ def readFile(path):
     print(f"Loading data from {path}")
     eoh = getEOH(path)
     eoh += 10  # skips first 10 lines
-    data = pd.read_csv(path, skiprows=eoh, header=None, sep="\t", usecols=[0,2, 3, 4], names=["Time","EEG", "fNIRS1", "fNIRS2"])
+    data = pd.read_csv(path, skiprows=eoh, header=None, sep="\t", usecols=[0, 1, 2, 3],
+                       names=["Time", "EEG", "fNIRS1", "fNIRS2"])
 
     try:
         file1 = open(path, 'r')
@@ -64,46 +72,59 @@ def moving_average(x, w):
     return np.convolve(x, np.ones(w), 'valid') / w
 
 
+def FNIRsToSpO2(IR, red, window):
+    IR_rolling = IR.rolling(window)
+    red_rolling = red.rolling(window)
+    IR_avg = IR_rolling.mean().fillna(0).to_numpy()
+    red_avg = red_rolling.mean().fillna(0).to_numpy()
+    IR_min = IR_rolling.min().to_numpy()
+    IR_max = IR_rolling.max().fillna(0).to_numpy()
+    red_min = red_rolling.min().fillna(0).to_numpy()
+    red_max = red_rolling.max().fillna(0).to_numpy()
+    IR_vpp = IR_max - IR_min
+    red_vpp = red_max - red_min
+    R = (red_vpp * IR_avg) / (red_avg * IR_vpp)
+    SpO2 = 110 - 25 * R
+    Sp02Rev = ((SpO2 * 95) / SpO2[0])
+    return SpO2[::window], Sp02Rev[::window]
+
+
 def displayData(df, sr):
     # yasa.plot_spectrogram(df["EEG"].to_numpy(), win_sec=1, sf=1000, cmap='Spectral_r')
     # plt.xlabel("Time[S]")
-    time = np.array(df["Time"])/sr
+    time = np.array(df["Time"]) / sr
     resolution = 16  # Resolution (number of available bits)
     signal_red_uA = (0.15 * np.array(df["fNIRS1"])) / 2 ** resolution
     signal_infrared_uA = (0.15 * np.array(df["fNIRS1"])) / 2 ** resolution
+
     eeg = EEGTransferFunction(df["EEG"])
     plt.figure(0)
     # Plot EEG
     plt.subplot(4, 1, 1)
-    plt.plot(time,eeg)
+    plt.plot(time, eeg)
 
     # Plot fNIRS1
     plt.subplot(4, 1, 2)
-    plt.plot(time,signal_red_uA)
+    plt.plot(time, signal_red_uA)
 
     # Plot fNIRS2
     plt.subplot(4, 1, 3)
-    plt.plot(time,signal_infrared_uA)
+    plt.plot(time, signal_infrared_uA)
 
     ax = plt.subplot(4, 1, 4)
-    avg_red = moving_average(signal_infrared_uA, 100)
-    avg_ir = moving_average(signal_infrared_uA, 100)
-    ax.plot(avg_red[0]-avg_red)
-    ax.plot(avg_ir-avg_ir[0])
+    avg_red = moving_average(signal_infrared_uA, 100)[::sr]
+    avg_ir = moving_average(signal_infrared_uA, 100)[::sr]
+    ax.plot(avg_red[0] - avg_red)
+    ax.plot(avg_ir - avg_ir[0])
 
     plotSpectrogram(eeg, sr)
 
-    #lp = np.hamming(35) / np.sum(np.hamming(35))
-    #eeg = eeg - filtfilt(lp, 1, eeg)
-
-    # plt.figure()
-    # plt.subplot(2, 1, 1)
-    # plt.magnitude_spectrum(eeg[10 * sr:50 * sr],Fs=sr)
-    # plt.xlim([0,40])
-    # plt.subplot(2, 1, 2)
-    # plt.magnitude_spectrum(eeg[70 * sr:110 * sr],Fs=sr)
-    # plt.xlim([0,40])
-
+    plt.figure()
+    SpO2, Sp02Rev = FNIRsToSpO2(df["fNIRS2"], df["fNIRS1"], sr)
+    ax = plt.subplot(1, 1, 1)
+    ax.plot(SpO2)
+    ax.plot(moving_average(SpO2, 5))
+    plt.ylim([85, 99])
     plt.show()
 
 
