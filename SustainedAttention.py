b650d89b6a6053e748aa1243607b8d961d6323f5
Gregory-Marshall <90696781+Gregory-Marshall@users.noreply.github.com>
2021-12-14 17:18:23 -0500

    Scatter plots

--- SustainedAttention.py
+++ SustainedAttention.py
@@ -69,7 +69,7 @@ def readBioSignalFile(path):
         headerJson = headerJson[list(headerJson)[0]]
         fs = headerJson["sampling rate"]
         tstring = headerJson["time"]
-        date_object = datetime.strptime("2021:"+tstring, "%Y:%H:%M:%S.%f")
+        date_object = datetime.strptime("2000:" + tstring, "%Y:%H:%M:%S.%f")
         t0 = date_object.timestamp() * 1000
         file1.close()
     except KeyError:
@@ -140,53 +140,115 @@ def displayData(df, df_sa, sr):
     plt.xlabel("Time (S)")
     plt.ylabel("Amperage (uA)")
 
-    spectrogramFig, meanFig, aplha, beta, gamma = plotSpectrogram(eeg, sr, cpu_cores=1, window=[4, 1], res=1.5,
-                                                                  resample=False)
-
     SpO2Fig = plt.figure("SpO2 fNIRs")
     SpO2, SpO2Rev = FNIRsToSpO2(signal_infrared_uA, signal_red_uA, sr)
-    ax = plt.subplot(1, 1, 1)
     rolling_avg_SpO2 = moving_average(SpO2, 5)
-    for t in range(60, int(len(time) / sr), 60):
-        ax.axvline(x=t, linestyle='--', linewidth=1.5, color='blue')
     bnotch, anotch = signal.iirnotch(0.2, 2)
     rolling_w_notch_filter = signal.filtfilt(bnotch, anotch, rolling_avg_SpO2)
+    reaction = df_sa["Reaction"].to_numpy()
+    sa_times = df_sa["Time"].to_numpy()
     fNIRS_mean = []
     fNIRS_min = []
     fNIRS_max = []
-    n = math.ceil(time[len(time) - 1] / 10)  # length of min max and mean window in seconds
-    for i in np.arange(0, int(len(rolling_w_notch_filter) / n) + 1):
-        start = i * n
-        end = start + n
+    reaction_mean = []
+    reaction_var = []
+    window_length = 30  # length of min max and mean window in seconds
+    step_size = 2
+    for i in np.arange(0, len(rolling_w_notch_filter), int(step_size)):
+        start = i
+        end = min(start + window_length, len(rolling_w_notch_filter))
         slice = rolling_w_notch_filter[start:end]
+        if len(slice) == 0: break
         fNIRS_mean.append(np.mean(slice, axis=0))
         fNIRS_min.append(np.amin(slice, axis=0))
         fNIRS_max.append(np.amax(slice, axis=0))
-    fNIRS_times = np.arange(0, len(fNIRS_mean) * n, n)
-    lspo2, = ax.plot(rolling_w_notch_filter, color='red', linewidth=2)
-    lmean, = ax.plot(fNIRS_times, fNIRS_mean, linestyle='--', linewidth=.75, color='black')
-    lmin, = ax.plot(fNIRS_times, fNIRS_min, linewidth=4, color='black', alpha=.3)
-    lmax, = ax.plot(fNIRS_times, fNIRS_max, linewidth=4, color='black', alpha=.3)
-    ax.plot(df_sa["Time"], df_sa["Reaction"].to_numpy()+fNIRS_min[0]-3, linewidth=2, color='purple')
+        s_start = find_nearest_index(sa_times, start)
+        s_end = find_nearest_index(sa_times, end)
+        reaction_slice = reaction[s_start:s_end]
+        mean = np.mean(reaction_slice, axis=0)
+        var = np.std(reaction_slice, axis=0)
+        reaction_mean.append(mean)
+        reaction_var.append(var)
+    reaction_mean = np.array(reaction_mean)
+    reaction_var = np.array(reaction_var)
+    window_times = np.arange(0, len(fNIRS_mean)*step_size, step_size)
+
+    ax = plt.subplot(2, 1, 1)
+    for t in range(window_length, int(len(time) / sr), window_length):
+        ax.axvline(x=t, linestyle='--', linewidth=1, color='blue', alpha=.25)
+    lspo2, = ax.plot(rolling_w_notch_filter, color='red', linewidth=1.5, alpha=.6)
+    lmean, = ax.plot(window_times, fNIRS_mean, linestyle='--', linewidth=2.5, color='black')
+    lmin, = ax.plot(window_times, fNIRS_min, linewidth=4, color='black', alpha=.5)
+    lmax, = ax.plot(window_times, fNIRS_max, linewidth=4, color='black', alpha=.5)
     ax.legend([lspo2, lmean, lmin, lmax], ['Sp02', 'Average', 'Min', 'Max'])
     plt.xlabel("Time (S)")
     plt.ylabel("SpO2 (%)")
+    ax = plt.subplot(2, 1, 2)
+    for t in range(window_length, int(len(time) / sr), window_length):
+        ax.axvline(x=t, linestyle='--', linewidth=1, color='blue', alpha=.25)
+    lraw, = ax.plot(df_sa["Time"], reaction, linewidth=1, color='red')
+    lavg, = ax.plot(window_times, reaction_mean, linewidth=2, color='purple')
+    lvar, = ax.plot(window_times, reaction_var, linewidth=2, color='green')
+    ax.legend([lraw, lavg, lvar], ['Reaction Time', 'Mean', 'Variance'])
+
+    notNan = np.where(np.logical_not(np.isnan(reaction_mean)))[0]
+    first_val = notNan[0]
+    last_val = notNan[len(notNan) - 1]
+
+    scatterMean = plt.figure("ScatterPlot Mean")
+    ax = plt.subplot(1, 1, 1)
+    x = fNIRS_mean[first_val:last_val]
+    y = reaction_mean[first_val:last_val]
+    ax.scatter(x, y)
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    ax.plot(x, p(x), "r--")
+    correlation = np.corrcoef(x, y)[0, 1]
+    ax.text(x[0], p(x)[0], "R={:.3f}, Rsq={:.3f}".format(correlation, correlation ** 2))
+    scatterVar = plt.figure("ScatterPlot Variance")
+    ax = plt.subplot(1, 1, 1)
+    y = reaction_var[first_val:last_val]
+    ax.scatter(x, y)
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    ax.plot(x, p(x), "r--")
+    correlation = np.corrcoef(x, y)[0, 1]
+    ax.text(x[0], p(x)[0], "R={:.3f}, Rsq={:.3f}".format(correlation, correlation ** 2))
+
+    # scatterFig = plt.figure("ScatterPlot")
+    # ax = plt.subplot(1, 1, 1)
+    # resampled = np.array([find_nearest(SpO2, t) for t in df_sa["Time"]])
+    # x = df_sa["Reaction"]
+    # y = resampled
+    # ax.scatter(x, y, s=40, alpha=.5)
+    # z = np.polyfit(x, y, 1)
+    # p = np.poly1d(z)
+    # ax.plot(x, p(x), "r--")
     figs.append(rawFig)
     figs.append(SpO2Fig)
-    figs.append(spectrogramFig)
-    figs.append(meanFig)
+    figs.append(scatterMean)
+    figs.append(scatterVar)
     return figs
+
+
+def find_nearest_index(array, value):
+    array = np.abs(array - value)
+    idx = array.argmin()
+    return idx
+
+
+def toTime(s):
+    ret = ((datetime.strptime("2000:" + s, "%Y:%H:%M:%S.%f").timestamp() * 1000 - t0) / 1000)
+    return ret
 
 
 def readSustainedFile(path, t0, sr):
     print(f"Loading data from {path}")
     data = pd.read_csv(path, skiprows=1, header=None, usecols=[0, 18, 19, 20],
                        names=["Time", "Input", "Correct", "Reaction"])
-    timeArr = data["Time"].to_numpy()
-    toTime = lambda s: ((datetime.strptime("2021:"+s, "%Y:%H:%M:%S.%f").timestamp() * 1000 - t0)/1000)
-    timeArr = np.array([toTime(s) for s in timeArr])
-    data["Time"] = pd.Series(timeArr)
-    return data
+    # data = data[data["Reaction"] != -1]
+    data["Time"] = pd.Series([toTime(s) for s in data["Time"]])
+    return data[data["Reaction"] != -1]
 
 
 if __name__ == '__main__':
